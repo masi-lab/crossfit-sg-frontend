0d91b9a5bd1dc5877d170b79b323b79d22d8f7df
Cinsua <insua35641@gmail.com>
2020-02-07 12:00:37 -0300

    students form started

---
+++ src/Components/Students/StudentForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Input, Pagination, Table, Avatar, Button, Modal } from 'antd';
+import useStyles from '../Common/styles'
+
+const StudentForm = () => {
+  return (
+    <div style={{
+      height: '100%', width: '100%',
+      display: 'grid',
+      gridTemplateColumns: 'minmax(1fr, 1fr)',// almost equals to '50% 50%',
+      gridTemplateRows: '40px 40px 40px 40px 40px 40px', // almost equal to '100%',
+      gridGap: '15px'
+    }}>
+
+    </div>
+  )
+}
+export default StudentForm;
